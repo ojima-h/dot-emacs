be9d46184ff92d88184945ab66c8229cf6a14b2c
Hikaru Ojima <ojima.h@gmail.com>
2015-11-24 08:52:55 +0900

    update

--- init.el
+++ init.el
@@ -7,7 +7,22 @@
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+;;
+;; set PATH
+;;
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(set-exec-path-from-shell-PATH)
+
+;;
+;; el-et
+;;
 (el-get 'sync)
 
 (el-get-bundle init-loader)
@@ -17,6 +32,8 @@
 (el-get-bundle sequential-command)
 (el-get-bundle sequential-command-config)
 (el-get-bundle auto-complete)
+(el-get-bundle flycheck)
+;(el-get-bundle foreign-regexp)
 
 (require 'init-loader)
 (init-loader-load)
